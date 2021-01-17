d6a5b50444a3e0163e7dde6c64b599daf3b70c97
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-17 23:43:39 +0100

    updated imagelinkform

--- src/components/ImageLinkForm/imageLinkForm.js
+++ src/components/ImageLinkForm/imageLinkForm.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import 'tachyons'
 import './imageLinkForm.css'
-import Tilt from 'react-tilt'
-import loadingimg from "../../loadingimg.svg"
+import loadingimg from '../../loadingimg.svg'
 
 const ImageLinkForm = ({
   handleChange,
@@ -19,7 +18,11 @@ const ImageLinkForm = ({
           <div className=" center celebName ml2 ph2">
             <p className="f4 ttc">
               <span className="textCeleb flex">{text}</span>{' '}
-              {loading ? <img className="center" src={loadingimg} alt="loading"/> : celebName}
+              {loading ? (
+                <img className="center" src={loadingimg} alt="loading" />
+              ) : (
+                celebName
+              )}
             </p>
           </div>
         ) : (
@@ -28,24 +31,22 @@ const ImageLinkForm = ({
             detect their name`}
           </p>
         )}
-        <Tilt className="center" options={{ max: 55 }}>
-          <div className="form  pa3 tc br3 shadow-5">
-            <div>
-              <input
-                className="input f4 pa2 w-70 center"
-                type="text"
-                placeholder="Enter image link to detect"
-                onChange={handleChange}
-              />
-              <button
-                onClick={handleClick}
-                className="imageLinkBtn btn w-30 mt3 grow f4 link ph3 pv2 dib white"
-              >
-                Detect
-              </button>
-            </div>
+        <div className="form center pa3 tc br3 shadow-4">
+          <div>
+            <input
+              className="f4 pa2 w-100 center"
+              type="text"
+              placeholder="Enter image link to detect"
+              onChange={handleChange}
+            />
+            <button
+              onClick={handleClick}
+              className="imageLinkBtn btn w-40 mt3 grow f4 link ph3 pv2"
+            >
+              Detect
+            </button>
           </div>
-        </Tilt>
+        </div>
       </section>
     </>
   )
